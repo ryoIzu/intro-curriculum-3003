a26283cb3c2dc0d715af324cee58b2dfb7ea7614
ryo izuta <r.izuta@mplpl.com>
2024-04-14 18:50:01 +0900

    ⚡️ submit answer

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ rl.on('close', () => {
     value.change = value.after / value.before;
   }
   const rankingArray = Array.from(prefectureDataMap).sort((pair1, pair2) => {
-    return pair2[1].change - pair1[1].change;
+    return pair1[1].change - pair2[1].change;
   });
   const rankingStrings = rankingArray.map(([key, value]) => {
     return `${key}: ${value.before}=>${value.after} 変化率: ${value.change}`;
